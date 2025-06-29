763160177cdd8f4207d903cc8188d807624b6393
Kums-sk <kumsiphone1@gmail.com>
2025-06-29 22:17:54 +0530

    reate_project.py

--- create_project.py
+++ create_project.py
@@ -1,25 +1,19 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2639
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww28600\viewh14900\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+import os
 
-\f0\fs24 \cf0 Import os\
-\
-#Define the new directory and file name\
-new_directory = \'93my_new_folder\'94\
-new_file = \'93my_first_file.txt\'94\
-\
-# Create the new directory\
-os.makedirs(new_directory, exist_ok=True)\
-print(f\'94Directory \'91\{new_directory\}\'92 created successfully\'94)\
-\
-#Define the content of the file\
-file_content = \'93Hello, DevOps world! This is my first Python script.\'94\
-\
-#Create and write to the new file\
-With open(os.path.join(new_directory, new_file), \'93w\'94) as f:\
-f.write(file_content)\
-\
-print(f\'94File \'91\{new_file\}\'92 created in \'91\{new_directory\}\'92.\'94)}
+# Define the new directory and file name
+new_directory = "my_new_folder"
+new_file = "my_first_file.txt"
+
+# Create the new directory
+os.makedirs(new_directory, exist_ok=True)
+print(f"Directory '{new_directory}' created successfully.")
+
+# Define the content of the file
+file_content = "Hello, DevOps world! This is my first Python script."
+
+# Create and write to the new file
+with open(os.path.join(new_directory, new_file), "w") as f:
+    f.write(file_content)
+
+print(f"File '{new_file}' created in '{new_directory}'.")
+
